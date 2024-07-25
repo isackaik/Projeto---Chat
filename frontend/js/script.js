a91ac50e58d49c43004384bf2c08868c5312ff7e
Isac Kaik <isackaik1@gmail.com>
2024-07-24 22:56:18 -0300

    update

---
+++ frontend/js/script.js
@@ -0,0 +1,19 @@
+// login elements
+const login = document.querySelector(".login");
+const loginForm = login.querySelector(".login_form");
+const loginInput = login.querySelector(".login_button");
+
+const user = {
+    id: "",
+    name: "",
+    color: ""
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    user.name = 
+}
+
+loginForm.addEventListener("submit", () => {
+
+});
